b2d90ead7dc19b2cb6b7018e814038c039cacad0
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-01-09 14:56:43 -0600

    Adding Controls for Commands

--- MAUIBeginner2023/App.xaml.cs
+++ MAUIBeginner2023/App.xaml.cs
@@ -10,7 +10,7 @@ public partial class App : Application
 		// navPage.BarBackgroundColor = Colors.Chocolate;
 		// navPage.BarTextColor = Colors.White;
 		// MainPage = navPage;
-		MainPage = new NavigationPage(new PresentationControlsDemo());
+		MainPage = new NavigationPage(new CommandsControlsDemo());
 	}
 }
 
---
+++ MAUIBeginner2023/CommandsControlsDemo.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MAUIBeginner2023;
+
+public partial class CommandsControlsDemo : ContentPage
+{
+	public CommandsControlsDemo()
+	{
+		InitializeComponent();
+	}
+
+    void btnTest_Clicked(System.Object sender, System.EventArgs e)
+    {
+		DisplayAlert("Test", "this is a demo", "ok");
+    }
+
+    void RadioButton_CheckedChanged(System.Object sender, Microsoft.Maui.Controls.CheckedChangedEventArgs e)
+    {
+        DisplayAlert("radio", "test", "ok");
+    }
+
+    void SwipeItem_Invoked(System.Object sender, System.EventArgs e)
+    {
+        DisplayAlert("radio", "element tapped", "ok");
+    }
+}
